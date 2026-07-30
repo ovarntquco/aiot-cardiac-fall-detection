00fbdb03bc3a5ed623ddbddd41790769d9e37a75
ovarntquco <duy.ovarntquco@gmail.com>
2026-07-30 17:11:33 +0700

    ngu

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -21,13 +21,10 @@ export const env = {
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-<<<<<<< HEAD
-=======
   MQTT_HOST: process.env.MQTT_HOST,
   MQTT_USERNAME: process.env.MQTT_USERNAME,
   MQTT_PASSWORD: process.env.MQTT_PASSWORD,
   MQTT_TOPICS: process.env.MQTT_TOPICS,
   MQTT_PORT: process.env.MQTT_PORT,
   SUPABASE_PRIVATE_CHANNEL: process.env.SUPABASE_PRIVATE_CHANNEL
->>>>>>> b1707b4413818e797312e58af84f9c8f6db5db64
 };
--- backend/src/models/cardiac.model.js
+++ backend/src/models/cardiac.model.js
@@ -1,8 +1,8 @@
-import supabase from "../config/supabase";
+import supabase from '../config/supabase';
 
 export async function create({device_id, heart_rate, spo2, recorded_at}) {
     const { data, error } = await supabase
-        .from("cardiac_readings")
+        .from('cardiac_readings')
         .insert({
             device_id: device_id,
             heart_rate: heart_rate,
