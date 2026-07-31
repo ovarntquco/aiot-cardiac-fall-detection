83c17e13b56f95a2877205acd59d33cd44710a67
Akuser0509 <nguyenminhkhoi2329@gmail.com>
2026-07-31 16:42:40 +0700

    add telegram api

--- backend/src/models/alert.model.js
+++ backend/src/models/alert.model.js
@@ -1,4 +1,5 @@
 import supabase from "../config/supabase";
+import { env } from "../config/env";
 
 export async function create({ eventId, type }) {
   const { data, error } = await supabase
@@ -32,4 +33,29 @@ export async function findById(device_id) {
     }
 
     return data;
+}
+
+let config = {
+  method: 'POST',
+  header: { 'Content-Type': 'application/json' },
+  body: undefined
+};
+
+export async function sendTelegram(chat_id) {
+  const url = `https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`;
+
+  config.body = JSON.stringify({
+    chat_id: chat_id,
+    text: "[SOS] - Alerts gone off"
+  });
+
+  const response = await fetch(url, config)
+
+  const data = await response.json();
+
+  if (!data.ok) {
+    throw new Error(`Can't send message to user`);
+  }
+
+  return data.result;
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import { env } from "./config/env.js";
 import app from "./app.js";
+import { registerWebhook } from "./config/telegram.js";
+
+await registerWebhook();
 
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT} [${env.NODE_ENV}]`);
